c140b5d5c408efef2ec01c9db7d6aff6a3ec7310
Berk <berk.guler@windowslive.com>
2021-04-15 23:32:36 +0300

    added endpoints

---
+++ src/services/PlacesService.ts
@@ -0,0 +1,24 @@
+
+import { PrismaClient, Session, User, Business } from '.prisma/client';
+import {Express, Router, Response, Request} from 'express';
+import moment from 'moment';
+import { textChangeRangeIsUnchanged } from 'typescript';
+
+export default class UserService {
+    public app: Express;
+    public client: PrismaClient;
+    public router: Router;
+
+    constructor(app: Express, client: PrismaClient) {
+        this.app = app;
+        this.client = client;
+        this.router = Router();
+
+        // GET: Places
+        this.router.get('/', async(req: Request, res: Response) =>{
+            let places = await this.client.business.findMany(); //get all business
+            res.send(places);
+        });
+        this.app.use('/places', this.router);
+    }
+}
