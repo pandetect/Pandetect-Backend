f63b7a7ad5b891995f358ba08910c243ff973f9e
Ufuk Bombar <ufuk.bombar@bordatech.com>
2021-04-17 12:27:47 +0300

    Hotfix businessStatistics

--- src/services/StatisticsService.ts
+++ src/services/StatisticsService.ts
@@ -1,5 +1,5 @@
 
-import { PrismaClient, Session, User, BusinessStatistic, Business } from '.prisma/client';
+import { PrismaClient, Session, User, Business } from '.prisma/client';
 import {Express, Router, Response, Request, json} from 'express';
 import moment from 'moment';
 import { textChangeRangeIsUnchanged } from 'typescript';
@@ -16,33 +16,16 @@ export default class StatisticsService{
         
         this.router.get('/:placename', async(req: Request, res: Response) =>{
             console.log("place name is: ", req.params.placename);
-            //let statistics = await this.getStatistics(req.params.placeName);
-            //return res.json(req.params.placename);
+
+
             if ( req.params.placename == undefined){
                 res.status(400);
                 return res.send("Invalid placename something");
             }
-            let stat = await this.getStatistics(req.params.placename);
-            return res.json(stat);
-            
+
+            return;
         });
 
         this.app.use('/statistics', this.router);
-    }
-
-    private async getStatistics(placeName: string): Promise<BusinessStatistic[]| undefined>{
-        const business= await this.client.business.findUnique({
-            where: {
-                name: placeName
-            }
-        });
-        let id = business?.uuid;
-
-        let businessStatistics = await this.client.businessStatistic.findMany({
-            where: {
-                uuid: id
-            }
-        });
-        return businessStatistics;
     }
 }
